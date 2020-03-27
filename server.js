3e1e52b734359234a4a2de1b818d70f36a37a05a
Robin Solanki <web.guru@me.com>
2020-03-28 03:22:33 +0530

    Configure 'config' module and add mongoDB atlas connection

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db');
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => {
-  res.send('API running')
-})
+	res.send('API running');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server started on port: ${PORT}`)
-})
+	console.log(`Server started on port: ${PORT}`);
+});
