5f6b269d35b6555a4940ee1375ae491aa04390f4
Robin Solanki <web.guru@me.com>
2020-04-01 07:29:31 +0530

    Configure Delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -118,7 +118,7 @@ router.post(
 
 // @route GET api/profile
 // @desc Get all profiles
-// @access Private
+// @access Public
 router.get('/', async (req, res) => {
 	try {
 		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
@@ -132,7 +132,7 @@ router.get('/', async (req, res) => {
 
 // @route GET api/profile/user/:user_id
 // @desc Get profile by user ID
-// @access Private
+// @access Public
 router.get('/user/:user_id', async (req, res) => {
 	try {
 		const profile = await Profile.findOne({
@@ -148,6 +148,24 @@ router.get('/user/:user_id', async (req, res) => {
 			return res.status(400).json({ msg: 'Profile not found' });
 		}
 
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route DELETE api/profile
+// @desc Delete profile, user & posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+	try {
+		// @todo - remove users posts
+		// Remove profile
+		await Profile.findOneAndRemove({ user: req.user.id });
+		// Remove User
+		await User.findOneAndRemove({ _id: req.user.id });
+
+		res.json({ msg: 'User deleted' });
+	} catch (error) {
+		console.error(error.message);
 		res.status(500).send('Server Error');
 	}
 });
