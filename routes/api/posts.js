1ff6019223a092354801ced57b19c7848f4eee9d
Robin Solanki <web.guru@me.com>
2020-04-02 00:50:46 +0530

    Configure get single post by id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -50,13 +50,35 @@ router.post(
 
 // @route GET api/posts
 // @desc Get all posts
-// @access Public
+// @access Private
 router.get('/', auth, async (req, res) => {
 	try {
 		const posts = await Post.find().sort({ date: -1 });
 
 		res.json(posts);
 	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route GET api/posts/:id
+// @desc Get post by ID
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
+		res.json(post);
+	} catch (error) {
+		if (error.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
 		console.error(error.message);
 		res.status(500).send('Server Error');
 	}
