5d4218cd90ef8739d9af27f3f0d5f822c33378ed
Robin Solanki <web.guru@me.com>
2020-04-05 09:27:49 +0530

    configure alert component and action call

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Landing from './components/layouts/Landing';
 
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layouts/Alert';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -20,6 +21,7 @@ const App = () => (
 				<Navbar />
 				<Route exact path='/' component={Landing} />
 				<section className='container'>
+					<Alert />
 					<Switch>
 						<Route exact path='/register' component={Register} />
 						<Route exact path='/login' component={Login} />
