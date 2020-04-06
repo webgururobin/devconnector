5d7a0cadbbad7f2303275c7373b1e72722ae7246
Robin Solanki <web.guru@me.com>
2020-04-07 03:42:40 +0530

    Configure auth reducer and auth action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
@@ -24,7 +25,7 @@ const Register = ({ setAlert }) => {
 		if (password !== password2) {
 			setAlert('Passwords do not match', 'danger');
 		} else {
-			console.log('Success');
+			register({ name, email, password });
 		}
 	};
 
@@ -64,10 +65,10 @@ const Register = ({ setAlert }) => {
 						type='password'
 						placeholder='Password'
 						name='password'
-						minLength='6'
 						value={password}
 						onChange={(e) => onChange(e)}
 						required
+						minLength='6'
 					/>
 				</div>
 				<div className='form-group'>
@@ -75,10 +76,10 @@ const Register = ({ setAlert }) => {
 						type='password'
 						placeholder='Confirm Password'
 						name='password2'
-						minLength='6'
 						value={password2}
 						onChange={(e) => onChange(e)}
 						required
+						minLength='6'
 					/>
 				</div>
 				<input type='submit' className='btn btn-primary' value='Register' />
@@ -92,6 +93,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
 	setAlert: PropTypes.func.isRequired,
+	register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
