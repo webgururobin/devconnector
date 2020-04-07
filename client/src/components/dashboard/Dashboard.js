96e2d91b446180d90f1339cf8e7a3117e4d1043f
Robin Solanki <web.guru@me.com>
2020-04-08 05:10:46 +0530

    Add list eductaion and experience functionality

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 
 import Spinner from '../layouts/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({
 	getCurrentProfile,
@@ -29,6 +31,8 @@ const Dashboard = ({
 			{profile !== null ? (
 				<Fragment>
 					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
 				</Fragment>
 			) : (
 				<Fragment>
--- client/src/components/profile-forms/AddExperience.js
+++ client/src/components/profile-forms/AddExperience.js
@@ -1,7 +1,7 @@
 import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import { addExperience } from '../../actions/profile';
 
