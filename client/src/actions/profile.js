2a4a70d8bf6838206e53e2f5fc72b2c385e085a5
Robin Solanki <web.guru@me.com>
2020-04-08 10:07:59 +0530

    Complete profile action and reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,10 +3,12 @@ import { setAlert } from './alert';
 
 import {
 	GET_PROFILE,
+	GET_PROFILES,
 	PROFILE_ERROR,
 	UPDATE_PROFILE,
 	ACCOUNT_DELETED,
 	CLEAR_PROFILE,
+	GET_REPOS,
 } from './types';
 
 // Get current user's profile
@@ -17,6 +19,70 @@ export const getCurrentProfile = () => async (dispatch) => {
 
 		dispatch({
 			type: GET_PROFILE,
+			payload: res.data,
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {
+				msg: error.response.statusText,
+				status: error.response.status,
+			},
+		});
+	}
+};
+
+// get all profiles
+export const getProfiles = () => async (dispatch) => {
+	dispatch({
+		type: CLEAR_PROFILE,
+	});
+
+	try {
+		const res = await axios.get('/api/profile');
+
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data,
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {
+				msg: error.response.statusText,
+				status: error.response.status,
+			},
+		});
+	}
+};
+
+// Get profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+	try {
+		const res = await axios.get(`/api/profile/user/${userId}`);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data,
+		});
+	} catch (error) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: {
+				msg: error.response.statusText,
+				status: error.response.status,
+			},
+		});
+	}
+};
+
+// Get github Repos
+export const getGirhubRepos = (githubUsername) => async (dispatch) => {
+	try {
+		const res = await axios.get(`/api/profile/github/${githubUsername}`);
+
+		dispatch({
+			type: GET_REPOS,
 			payload: res.data,
 		});
 	} catch (error) {
