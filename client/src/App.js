2d1d4a0628766397e328f06dfbbb27d707198ee2
Robin Solanki <web.guru@me.com>
2020-04-08 10:31:03 +0530

    Configure get all profiles

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 
+import Profiles from './components/profiles/Profiles';
+
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import setAuthToken from './utils/setAuthToken';
@@ -44,6 +46,7 @@ const App = () => {
 						<Switch>
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
+							<Route exact path='/profiles' component={Profiles} />
 							<PrivateRoute exact path='/dashboard' component={Dashboard} />
 							<PrivateRoute
 								exact
